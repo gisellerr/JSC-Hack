064db1b268a2dbd772d497055a4fa0b245151895
Sergio Robledo <robledo.sergio18@gmail.com>
2022-01-29 09:20:51 -0600

    adding items.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ButtonAppBar from "./mui/menuBar";
 import Notepad from "./mui/Notepad";
 import Background from "./mui/Background";
+import Item from "./mui/Item";
 
 function App() {
   return (
@@ -10,8 +11,6 @@ function App() {
       
       <h1>Hello World</h1>
       {/*<Notepad/>*/}
-      
-      
     </div>
   );
 }
---
+++ src/mui/Item.js
@@ -0,0 +1,11 @@
+import Checkbox  from "./Checkbox";
+
+function Item() {
+    return (
+        <div>
+            <Checkbox/> 
+        </div>
+    );
+  }
+  
+  export default Item;
